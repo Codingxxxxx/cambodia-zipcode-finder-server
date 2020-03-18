8b645a58e2717fe6fdba9362f9ef80675f9424f0
codingxxxxx <hc2.minea@gmail.com>
2020-03-18 19:39:57 +0700

    added api

--- src/repository/mongodb.repository.js
+++ src/repository/mongodb.repository.js
@@ -1,9 +1,19 @@
 const {Pool} = require('pg');
-const {activeMode, dev} = require('../app.config');
+const {activeMode, dev, prod} = require('../app.config');
 
 const pool = new Pool((() => {
    if (activeMode === 'dev') {
       const {user, password, port, host, dbName} = dev.db;
+      return {
+         user: user,
+         host: host,
+         database: dbName,
+         password: password,
+         port: port,
+         max: 20
+      }
+   } else if (activeMode === 'prod') {
+      const {user, password, port, host, dbName} = prod.db;
       return {
          user: user,
          host: host,
--- src/server.js
+++ src/server.js
@@ -2,11 +2,9 @@ const server = require('express')();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const {apiController} = require('./controller/api.controller');
-const {checkAuthMiddleware} = require('./middleware/auth.middleware');
 
 server.use(cors());
 server.use(bodyParser.urlencoded({extended: true}));
 server.use(bodyParser.json());
-server.use(checkAuthMiddleware);
 server.use(apiController);
 server.listen(3001);
